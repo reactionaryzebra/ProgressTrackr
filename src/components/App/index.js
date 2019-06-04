a5a4328261055e4fb0a0301b00aa57a91fc40de2
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-06-04 08:50:43 -0700

    Set authUser in App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { FirebaseContext } from "../Firebase";
 
 import NavBar from "../NavBar";
 import RegistrationPage from "../Registration";
@@ -7,18 +8,37 @@ import LoginPage from "../Login";
 
 import * as ROUTES from "../../constants/routes";
 
+class AppBase extends Component {
+  state = {
+    authUser: null
+  };
+
+  componentDidMount() {
+    this.props.firebase.auth.onAuthStateChanged(authUser =>
+      authUser ? this.setState({ authUser }) : this.setState({ authUser: null })
+    );
+  }
+
+  render() {
+    return (
+      <Router>
+        <NavBar authUser={this.state.authUser} />
+
+        <Route exact path={ROUTES.REGISTER} component={RegistrationPage} />
+        <Route exact path={ROUTES.LOGIN} component={LoginPage} />
+        <Route
+          exact
+          path={ROUTES.HOME}
+          render={() => <div>This is the Home page</div>}
+        />
+      </Router>
+    );
+  }
+}
+
 const App = () => (
-  <Router>
-    <NavBar />
-
-    <Route exact path={ROUTES.REGISTER} component={RegistrationPage} />
-    <Route exact path={ROUTES.LOGIN} component={LoginPage} />
-    <Route
-      exact
-      path={ROUTES.HOME}
-      render={() => <div>This is the Home page</div>}
-    />
-  </Router>
+  <FirebaseContext.Consumer>
+    {firebase => <AppBase firebase={firebase} />}
+  </FirebaseContext.Consumer>
 );
-
 export default App;
