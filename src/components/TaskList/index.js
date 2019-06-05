c4776f8a9e2e5adcaf118918345bb407f87a52f0
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-06-05 13:08:06 -0700

    Add logic to display teacher's tasks in TaskList

--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -3,8 +3,48 @@ import { withFirebase } from "../Firebase";
 import { withAuthUser } from "../Session";
 
 class TaskListBase extends Component {
+  state = {
+    tasks: [],
+    adding: false,
+    error: null
+  };
+
+  setAdding = bool => {
+    this.setState({ adding: bool });
+  };
+
+  fetchTasks = async () => {
+    const { firebase, authUser } = this.props;
+    const tasksDB = firebase.db.collection("tasks");
+    try {
+      const response = await tasksDB
+        .where("createdBy", "==", authUser.uid)
+        .onSnapshot(snapshot => this.setState({ tasks: snapshot.docs }));
+    } catch (error) {
+      this.setState({ error });
+    }
+  };
+
+  componentDidMount() {
+    this.fetchTasks();
+  }
+
   render() {
-    <div>This is the Task list</div>;
+    return (
+      <div>
+        <h2>Tasks</h2>
+        <ul>
+          {this.state.tasks.length > 0
+            ? this.state.tasks.map((task, i) => (
+                <li key={i}>{task.data().title}</li>
+              ))
+            : null}
+        </ul>
+        <button onClick={() => this.setState({ adding: true })}>
+          Add Task
+        </button>
+      </div>
+    );
   }
 }
 
