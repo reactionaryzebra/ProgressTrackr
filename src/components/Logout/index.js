afec0306aa42d7c50ae46cca86c329113b763914
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-06-08 13:56:52 -0700

    Redirect to landing page on logout

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { withFirebase } from "../Firebase";
+import { withRouter } from "react-router-dom";
 
-const LogoutButtonBase = ({ firebase }) => (
-  <button type="button" onClick={firebase.doLogout}>
+const LogoutButtonBase = ({ firebase, history }) => (
+  <button
+    type="button"
+    onClick={() => {
+      firebase.doLogout();
+      history.push("/");
+    }}
+  >
     Logout
   </button>
 );
 
-const LogoutButton = withFirebase(LogoutButtonBase);
+const LogoutButton = withFirebase(withRouter(LogoutButtonBase));
 
 export default LogoutButton;
