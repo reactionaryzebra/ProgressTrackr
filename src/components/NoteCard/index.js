6528b703f48e92d8cc3594c6d0ce04350dda3361
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-06-08 13:29:43 -0700

    Add highestStep to NoteCard

--- src/components/NoteCard/index.js
+++ src/components/NoteCard/index.js
@@ -4,6 +4,7 @@ const NoteCard = ({ note }) => (
   <div>
     <h4>Written By: {note.createdByName}</h4>
     <h5>{note.date}</h5>
+    <h5>Highest step achieved alone: {note.highestStep}</h5>
     <p>{note.text}</p>
   </div>
 );
